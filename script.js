dd087c612a75189bc96b712c24dc3bbe561633aa
rhit-rameshr <143538062+rhit-rameshr@users.noreply.github.com>
2024-08-05 16:40:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,52 @@
-function addRecommendation() {
-  // Get the message of the new recommendation
-  let recommendation = document.getElementById("new_recommendation");
-  // If the user has left a recommendation, display a pop-up
-  if (recommendation.value != null && recommendation.value.trim() != "") {
-    console.log("New recommendation added");
-    //Call showPopup here
-    showPopup(true);
-    
+document.addEventListener("DOMContentLoaded", function () {
+  // Smooth scrolling for navigation links
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+  });
 
-    // Create a new 'recommendation' element and set it's value to the user's message
-    var element = document.createElement("div");
-    element.setAttribute("class","recommendation");
-    element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
-    // Add this element to the end of the list of recommendations
-    document.getElementById("all_recommendations").appendChild(element); 
-    
-    // Reset the value of the textarea
-    recommendation.value = "";
-  }
-}
+  // Function to show and hide popup
+  window.showPopup = function (show) {
+    document.getElementById('popup').style.visibility = show ? 'visible' : 'hidden';
+  };
+});
 
-function showPopup(bool) {
-  if (bool) {
-    document.getElementById('popup').style.visibility = 'visible'
-  } else {
-    document.getElementById('popup').style.visibility = 'hidden'
-  }
-}
+// Assuming you have jQuery included, if not, you can use vanilla JavaScript
+$('#send-message-btn').click(function() {
+    // Assuming you are using Formspree for this example
+    // Replace 'your_formspree_email' with your actual Formspree email
+    var formUrl = 'https://formspree.io/your_formspree_email';
+
+    // Assuming you want to collect user information to send
+    var message = prompt('Please enter your message:');
+    var user = {
+        name: prompt('Your name:'),
+        email: prompt('Your email:')
+    };
+
+    // Prepare data to send
+    var data = {
+        name: user.name,
+        replyto: user.email,
+        message: message
+    };
+
+    // Send the message via Formspree
+    $.ajax({
+        type: 'POST',
+        url: formUrl,
+        data: data,
+        dataType: 'json'
+    })
+    .done(function(response) {
+        alert('Message sent successfully!');
+    })
+    .fail(function(error) {
+        alert('Oops! Something went wrong. Please try again later.');
+    });
+});
+
